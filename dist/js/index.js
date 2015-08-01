d11687dae59d1fc7a34ff3ee86bf5bffc8afa957
nikhil1231 <nikhil1231@hotmail.co.uk>
2015-08-01 17:45:33 +0100

    k

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,6 +3,7 @@ $('document').ready(function(){
 		$.getJSON('http://api.open-notify.org/iss-now.json?callback=?',function(data){
 			$('#lat').html(data.iss_position.latitude);
 			$('#lon').html(data.iss_position.longitude);
+			$('#googlemap').attr('src',"https://maps.googleapis.com/maps/api/js?key=AIzaSyAF_ls43ayIqKQMbJFCDv6HtpKgEgvIEjg&q=loc:"+ data.iss_position.latitude +"+"+data.iss_position.longitude);
 		})
 	},2000)
 });
